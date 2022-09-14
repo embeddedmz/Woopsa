353476e61d9b1e18f9589fa8e78d5ee8daebcc69
Mohamed Amine Mzoughi <mmzoughi@jeanlutzsa.com>
2022-09-14 14:26:38 +0200

    Added a Woopsa client and a server to measure the update frequency.

---
+++ Sources/DotNet/WoopsaStressTestClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Woopsa;
+
+namespace WoopsaStressTestClient
+{
+    class Program
+    {
+        private static Stopwatch stopWatch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            ManualResetEvent quit = new ManualResetEvent(false);
+
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                // Tell .NET to not terminate the process
+                e.Cancel = true;
+
+                quit.Set();
+            };
+
+            string serverUrl = (args.Length > 0) ? args[0] : "http://localhost:80/woopsa/";
+
+            WoopsaClient client = new WoopsaClient(serverUrl);
+            Console.WriteLine("Woopsa client created on URL: {0}", serverUrl);
+
+            client.SubscriptionChannel.Subscribe("Temperature", Temperature_Changed);
+
+            // Leave the command prompt open
+            /*Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();*/
+
+            quit.WaitOne();
+
+            client.Dispose();
+        }
+
+        private static void Temperature_Changed(object sender, WoopsaNotificationEventArgs e)
+        {
+            stopWatch.Stop();
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            Console.WriteLine(String.Format("Delta T = {0}s - {1}ms", ts.Seconds, ts.Milliseconds));
+
+            //Console.WriteLine("Temperature value : {0}", e.Notification.Value.ToString());
+
+            stopWatch.Restart();
+        }
+    }
+}
---
+++ Sources/DotNet/WoopsaStressTestServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using Woopsa;
+
+namespace WoopsaStressTestServer
+{
+    public class WeatherStation
+    {
+        public double Temperature { get; set; }
+
+        public double Sensitivity { get; set; }
+
+        public string GetWeatherAtDate(DateTime date)
+        {
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    return "cloudy";
+                default:
+                    return "sunny";
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int updateFrequencyMs = (args.Length > 0) ? Convert.ToInt32(args[0]) : 250;
+                Console.WriteLine("Update frequency = {0}", updateFrequencyMs);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+
+                Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+                {
+                    // Tell .NET to not terminate the process
+                    e.Cancel = true;
+
+                    cts.Cancel();
+                };
+
+                WeatherStation station = new WeatherStation();
+                using (WoopsaServer woopsaServer = new WoopsaServer(station, 5000))
+                {
+                    Random randomTemperature = new Random();
+
+                    while (!cts.Token.IsCancellationRequested)
+                    {
+                        station.Temperature = randomTemperature.Next(0, 1000);
+                        Thread.Sleep(updateFrequencyMs);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e?.Message ?? "");
+            }
+        }
+    }
+}
